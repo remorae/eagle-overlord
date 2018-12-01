b1d62c01c48727878b0bb1e897b5b3de630f903e
natrastellar <96928162+natrastellar@users.noreply.github.com>
2018-11-30 18:10:06 -0800

    Stylistic changes
    Signed-off-by: natrastellar <96928162+natrastellar@users.noreply.github.com>

--- spec/bot.spec.js
+++ spec/bot.spec.js
@@ -1,4 +1,5 @@
-var bot = require('../bot.js');
+const bot = require('../bot.js');
+
 describe('bot isValidPrefix', () => {
     it('should allow "CSCD", "MATH", "EENG", "PHYS", "CHEM", "GEOL", "PHIL", "DESN"', () => {
         console.log(bot);
