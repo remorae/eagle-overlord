b32d59afcb5b771155dc5685a94ddfc22e3d0ceb
natrastellar <96928162+natrastellar@users.noreply.github.com>
2022-07-02 10:13:34 -0700

    fix(AboutCommand): missing package.json
    Signed-off-by: natrastellar <96928162+natrastellar@users.noreply.github.com>

--- src/client/commands/about.ts
+++ src/client/commands/about.ts
@@ -2,7 +2,7 @@ import type { SlashCommandBuilder } from '@discordjs/builders';
 import type { CommandInteraction } from 'discord.js';
 import type { ClientInstance } from '../../client.js';
 import config from '../../config.js';
-import { loadRelativeToMain } from '../../utils.js';
+import packageJson from '../../../package.json';
 import type { Command } from '../command.js';
 
 class AboutCommand implements Command {
@@ -12,9 +12,8 @@ class AboutCommand implements Command {
             .setDescription('Displays information about, well, me.');
     }
     async execute(interaction: CommandInteraction, _client: ClientInstance): Promise<void> {
-        const infoFile = loadRelativeToMain('../../package.json', false);
-        if (config && infoFile) {
-            await interaction.reply({ content: `Currently running on version ${infoFile.version}. Created in ${config.client.deployYear} by ${config.client.developerUserName}.`, allowedMentions: { users: [] } });
+        if (config && packageJson) {
+            await interaction.reply({ content: `Currently running on version ${packageJson.version}. Created in ${config.client.deployYear} by ${config.client.developerUserName}.`, allowedMentions: { users: [] } });
         }
         else if (config) {
             await interaction.reply({ content: 'Failed to load config file.' });
