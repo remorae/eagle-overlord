f02316cc86a5d429f1bf62a19248201c1d22e421
Mori <me@morine.net>
2022-07-06 20:55:14 -0700

    feat(EmbedCommand): Convert !embed to slash command
    Remove old message command parsing.
    Remove reaction event handling.
    
    Signed-off-by: natrastellar <96928162+natrastellar@users.noreply.github.com>

---
+++ src/client/commands/embed.ts
@@ -0,0 +1,154 @@
+import type { SlashCommandBuilder } from '@discordjs/builders';
+import { ApplicationCommandPermissionData, CommandInteraction, Guild, HexColorString, MessageActionRow, MessageEmbed, Modal, ModalActionRowComponent, ModalSubmitInteraction, Permissions, TextChannel, TextInputComponent, ThreadChannel } from 'discord.js';
+import type { ClientInstance } from '../../client.js';
+import { MILLIS_PER_SECOND } from '../../constants.js';
+import { Command, commandRolePermission, rolesWithPermissions } from '../command.js';
+
+class EmbedCommand implements Command {
+    async build(builder: SlashCommandBuilder): Promise<void> {
+        builder
+            .setName('embed')
+            .setDescription('Embed the given info in a new message in the given channel.')
+            .addChannelOption((option) =>
+                option
+                    .setName('channel')
+                    .setDescription('The text channel to send the message to.')
+                    .setRequired(true)
+            )
+            .addStringOption((option) =>
+                option
+                    .setName('title')
+                    .setDescription('The title of the embed.')
+                    .setRequired(true)
+            )
+            .addStringOption((option) =>
+                option
+                    .setName('color')
+                    .setDescription('The color of the embed, e.g. 0xff0000.')
+                    .setRequired(true)
+            )
+            .addStringOption((option) =>
+                option
+                    .setName('edit_id')
+                    .setDescription('The ID of the message to edit.')
+            )
+            .setDefaultPermission(false);
+    }
+    async getPermissions(guild: Guild, permissions: ApplicationCommandPermissionData[]) {
+        for (const role of rolesWithPermissions(guild, Permissions.FLAGS.MANAGE_CHANNELS)) {
+            permissions.push(commandRolePermission(role.id, true));
+        }
+    }
+    async execute(interaction: CommandInteraction, client: ClientInstance) {
+        if (!interaction.inGuild()) {
+            await interaction.reply({ content: 'This command can only be used in a server.', ephemeral: true });
+            return;
+        }
+        const channel = interaction.options.getChannel('channel', true);
+        if (!(channel instanceof TextChannel || channel instanceof ThreadChannel)) {
+            await interaction.reply({ content: 'Channel is not a text/thread channel.', ephemeral: true });
+            return;
+        }
+        const embed = await createEmbedFromOptions(interaction);
+        if (embed) {
+            const editID = interaction.options.getString('edit_id', false);
+            await askForEmbedDescription(interaction, client, channel, editID, embed);
+        }
+    }
+}
+
+export const command: Command = new EmbedCommand();
+
+async function createEmbedFromOptions(interaction: CommandInteraction) {
+    const title = interaction.options.getString('title', true);
+    const colorStr = interaction.options.getString('color', true);
+    let color = null;
+    try {
+        color = colorStr.startsWith('0x')
+            ? `#${colorStr.slice('0x'.length)}` as HexColorString
+            : parseInt(colorStr, 10);
+    }
+    catch (e) {
+    }
+    if (!color || (typeof color === "number" && Number.isNaN(color))) {
+        await interaction.reply({ content: 'Invalid color.', ephemeral: true });
+        return null;
+    }
+
+    const embed = new MessageEmbed()
+        .setTitle(title)
+        .setColor(color);
+    return embed;
+}
+
+type GuildTextBasedChannel = TextChannel | ThreadChannel;
+
+async function askForEmbedDescription(interaction: CommandInteraction, client: ClientInstance, channel: GuildTextBasedChannel, editID: string | null, embed: MessageEmbed) {
+    const TIMEOUT_SECONDS = 60;
+    await interaction.showModal(buildDescriptionModal());
+    let submission: ModalSubmitInteraction | null = null;
+    try {
+        submission = await interaction.awaitModalSubmit({
+            filter: async (i: ModalSubmitInteraction) => {
+                await i.deferUpdate();
+                return i.user.id === interaction.user.id;
+            },
+            time: TIMEOUT_SECONDS * MILLIS_PER_SECOND
+        });
+    }
+    catch (err) {
+        await interaction.followUp({ content: `Timed out waiting for reply after ${TIMEOUT_SECONDS} seconds.`, ephemeral: true });
+    }
+    if (submission) {
+        embed.setDescription(submission.fields.getTextInputValue('description'));
+        await sendEmbed(submission, client, channel, editID, embed);
+    }
+}
+
+function buildDescriptionModal() {
+    return new Modal()
+        .setCustomId('embedDescriptionModal')
+        .setTitle(`Embed`)
+        .addComponents(
+            new MessageActionRow<ModalActionRowComponent>()
+                .addComponents(
+                    new TextInputComponent()
+                        .setCustomId('description')
+                        .setLabel('Description')
+                        .setPlaceholder('Please enter the embed description...')
+                        .setStyle('PARAGRAPH')
+                        .setRequired(true)
+                )
+        );
+}
+
+async function sendEmbed(interaction: ModalSubmitInteraction, client: ClientInstance, channel: GuildTextBasedChannel, editID: string | null, embed: MessageEmbed) {
+    try {
+        if (editID) {
+            await editMessageWithEmbed(interaction, client, channel, editID, embed);
+        }
+        else {
+            await channel.send({ embeds: [embed] });
+            await interaction.followUp({ content: 'Done!', ephemeral: true });
+        }
+    }
+    catch (e) {
+        await client.reportError(e, 'EmbedCommand::execute');
+        await interaction.followUp({ content: 'Something went wrong!' });
+    }
+}
+
+async function editMessageWithEmbed(interaction: ModalSubmitInteraction, client: ClientInstance, channel: GuildTextBasedChannel, editID: string, embed: MessageEmbed) {
+    let messageToEdit = null;
+    try {
+        messageToEdit = await channel.messages.fetch(editID);
+    }
+    catch (e) {
+        await client.reportError(e);
+        await interaction.followUp({ content: 'Invalid message to edit.', ephemeral: true });
+    }
+    if (messageToEdit) {
+        await messageToEdit.edit({ embeds: [embed] });
+        await interaction.followUp({ content: 'Done!', ephemeral: true });
+    }
+}
--- src/utils.ts
+++ src/utils.ts
@@ -1,66 +1,5 @@
-import { Message, Guild, GuildMember, GuildChannel, ThreadChannel, User } from 'discord.js';
+import type { Guild, GuildChannel, ThreadChannel } from 'discord.js';
 import Path from 'path';
-
-export async function parseCachedUser(message: Message, arg: string): Promise<GuildMember | Iterable<GuildMember> | null> {
-    if (!message.guild) {
-        await message.channel.send('This command requires a guild.');
-        return null;
-    }
-    switch (arg) {
-        case null:
-            await message.channel.send('You must enter a user.');
-            return null;
-        case 'all':
-            return message.guild.members.cache.values();
-        default:
-            break;
-    }
-    let gm = getCachedMember(message.guild, arg);
-    if (!gm) {
-        gm = getMemberByUserName(message.guild, arg);
-    }
-    if (!gm && isQuoteDelimited(arg)) {
-        gm = getMemberByDisplayName(message.guild, arg.slice(1, -1));
-    }
-    return gm;
-}
-
-function isQuoteDelimited(arg: string) {
-    return arg.length > 2 &&
-        arg[0] === arg[-1] &&
-        (arg[0] === '"' || arg[0] === '\'' || arg[0] === '`');
-}
-
-export function parseCachedChannel(message: Message, arg: string): GuildChannel | ThreadChannel | null {
-    let ch = message.guild?.channels.cache.find(c => c.id === arg);
-    if (!ch) {
-        ch = message.guild?.channels.cache.find(c => c.name === arg);
-    }
-    if (!ch && isQuoteDelimited(arg)) {
-        ch = message.guild?.channels.cache.find(c => c.name === arg.slice(1, -1));
-    }
-    return ch ? ch : null;
-}
-
-export function getCachedMember(guild: Guild, user: string | User): GuildMember | null {
-    const id = user instanceof User ? user.id : user;
-    return guild.members.cache.get(id) ?? null;
-}
-
-export function getMemberByUserName(guild: Guild, name: string): GuildMember | null {
-    return guild.members.cache.find(m => m.user.username === name) ?? null;
-}
-
-export function getMemberByDisplayName(guild: Guild, name: string): GuildMember | null {
-    return guild.members.cache.find(m => m.displayName === name) ?? null;
-}
-
-export function getAuthorMember(message: Message): GuildMember | null {
-    if (message.guild) {
-        return getCachedMember(message.guild, message.author);
-    }
-    return null;
-}
 
 export function getCachedChannel(guild: Guild, channel: string): GuildChannel | ThreadChannel | null {
     return guild.channels.cache.get(channel) ?? null;
@@ -80,10 +19,10 @@ export function resolveRelativeToMain(relativePath: string): string | null {
     return null;
 }
 
-export function loadRelativeToMain(relativePath: string, reload: boolean) {
-    const absolutePath = resolveRelativeToMain(relativePath);
-    if (absolutePath) {
-        return loadAtRuntime(absolutePath, reload);
+export function escapeCodeBlocks(input: string) {
+    let escaped = input.replace(/```/g, '\\`\\`\\`');
+    if (escaped.endsWith('`')) {
+        escaped += ' ';
     }
-    return null;
+    return escaped;
 }
